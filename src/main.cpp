5def69e4e5fee73dd231dcbfa396031667a21b88
Cyrus Yiu <UnsignedArduino@outlook.com>
2024-05-21 00:12:47 -0400

    Print out CSV file with results of each game

--- src/main.cpp
+++ src/main.cpp
@@ -3,53 +3,110 @@
 #include <cstdio>
 #include <random>
 
-int main() {
-  const uint32_t seed = 0;
+//#define DEBUG
 
-  printf("Blackjack game (#%d)\n", seed);
+#ifdef DEBUG
+  #define logf(...) printf(__VA_ARGS__);
+#else
+  #define logf(...)
+#endif
+
+const uint8_t ACTION_HIT = 1;
+const uint8_t ACTION_STAND = 0;
+
+const int8_t RESULT_WIN = 1;
+const int8_t RESULT_LOSS = -1;
+const int8_t RESULT_TIE = 0;
+
+int8_t simulateGame(uint32_t seed, uint8_t action, card_t &sum) {
+  logf("Blackjack game (#%d)\n", seed);
   BlackjackGame game;
   game.setup(seed);
 
-  printf("Player hand: ");
+  logf("Player hand: ");
+#ifdef DEBUG
   game.playerHand.printHand();
+#endif
 
-  printf("\nDealer hand: ");
+  sum = game.playerHand.getHandValue();
+
+  logf("\nDealer hand: ");
+#ifdef DEBUG
   printCard(game.dealerHand.getCardFromTop());
-  printf("??  \n");
+#endif
+  logf("??  \n");
+
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "Simplify"
+  if (action == ACTION_HIT) {
+    logf("Hit\n");
+    game.playerHit();
+    logf("Player hand: ");
+#ifdef DEBUG
+    game.playerHand.printHand();
+#endif
+    logf("\n");
+  } else {
+    logf("Stand\n");
+    game.playerStand();
+  }
+#pragma clang diagnostic pop
+
+  std::mt19937 rng(seed);
+  std::uniform_int_distribution<uint8_t> dist(0, 1);
 
   while (game.gameState() == BLACKJACK_GAME_IN_PROGRESS) {
-    printf("Hit or stand? (h/s):");
-    char input;
-    scanf("%c", &input);
-
-    if (input == 'h') {
+    if (dist(rng) == ACTION_HIT) {
+      logf("Hit\n");
       game.playerHit();
-      printf("Player hand: ");
+      logf("Player hand: ");
+#ifdef DEBUG
       game.playerHand.printHand();
-      printf("\n");
-    } else if (input == 's') {
+#endif
+      logf("\n");
+    } else {
+      logf("Stand\n");
       break;
     }
   }
   game.playerStand();
 
-  printf("Dealer hand: ");
+  logf("Dealer hand: ");
+#ifdef DEBUG
   game.dealerHand.printHand();
-  printf("\n");
+#endif
+  logf("\n");
 
+#ifdef DEBUG
   switch (game.gameState()) {
     case BLACKJACK_GAME_PLAYER_WIN:
-      printf("Player wins!\n");
+      logf("Player wins!\n");
       break;
     case BLACKJACK_GAME_PLAYER_LOSS:
-      printf("Dealer wins!\n");
+      logf("Dealer wins!\n");
       break;
     case BLACKJACK_GAME_DRAW:
-      printf("Draw!\n");
+      logf("Draw!\n");
       break;
     default:
       break;
   }
+#endif
 
-  return 0;
+  return game.gameState();
+}
+
+int main() {
+  printf("seed, sum, action, result\n");
+
+  card_t sum;
+  int8_t result;
+  for (uint32_t seed = 0; seed < 1000; seed++) {
+    result = simulateGame(seed, ACTION_STAND, sum);
+    printf("%d, %d, STAND, %s\n", seed, sum, result == BLACKJACK_GAME_PLAYER_WIN ? "WIN" : result == BLACKJACK_GAME_PLAYER_LOSS ? "LOSS"
+                                                                                                                                : "TIE");
+    result = simulateGame(seed, ACTION_HIT, sum);
+    printf("%d, %d, HIT, %s\n", seed, sum, result == BLACKJACK_GAME_PLAYER_WIN ? "WIN" : result == BLACKJACK_GAME_PLAYER_LOSS ? "LOSS"
+                                                                                                                              : "TIE");
+  }
 }
