bcd68174e60264e9493fc75de512c1e0cb40ed7e
Cyrus Yiu <UnsignedArduino@outlook.com>
2024-05-25 23:15:48 -0400

    Change fitness function to money flow

--- src/main.cpp
+++ src/main.cpp
@@ -48,31 +48,19 @@ int8_t simulateGame(uint32_t seed, blackjack_game_move_t algorithmMatrix[AM_DEAL
   return game.moneyFlow();
 }
 
-int64_t fitness(blackjack_game_move_t algorithmMatrix[AM_DEALERS_SIZE][AM_PLAYERS_SIZE]) {
-  printf("Running fitness test on algorithm matrix\n");
-  uint32_t wins = 0, draws = 0, losses = 0;
+int32_t fitness(blackjack_game_move_t algorithmMatrix[AM_DEALERS_SIZE][AM_PLAYERS_SIZE]) {
+  //  printf("Running fitness test on algorithm matrix\n");
   int32_t moneyFlow = 0;
   for (uint32_t seed = 0; seed < FITNESS_GAME_SIMS; seed++) {
-    int8_t result = simulateGame(seed, algorithmMatrix);
-    moneyFlow += result;
-    if (result > 0) {
-      wins++;
-    } else if (result < 0) {
-      losses++;
-    } else {
-      draws++;
-    }
-    if (seed % 1000 == 0) {
-      printf("%.2f%% finished\n", std::round(((float) seed / FITNESS_GAME_SIMS) * 100));
-    }
+    moneyFlow += simulateGame(seed, algorithmMatrix);
+    //    if (seed % 1000 == 0) {
+    //      printf("%.2f%% finished\n", std::round(((float) seed / FITNESS_GAME_SIMS) * 100));
+    //    }
   }
-  int64_t fitness = wins * 2 + draws - losses + moneyFlow;
-  printf("%d wins (%.2f%%, %d draws (%.2f%%), %d losses (%.2f%%)\n", wins, (float) wins / FITNESS_GAME_SIMS * 100, draws, (float) draws / FITNESS_GAME_SIMS * 100, losses, (float) losses / FITNESS_GAME_SIMS * 100);
-  printf("Money flow: %d\n", moneyFlow);
-  printf("Fitness: %lld\n", fitness);
-  return fitness;
+  //  printf("Fitness: %d\n", moneyFlow);
+  return moneyFlow;
 }
 
 int main() {
-  fitness(perfect_algorithm_matrix);
+  printf("%d\n", fitness(perfect_algorithm_matrix));
 }
